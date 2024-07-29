68a53fb1f960ff0d57dafb67579051deabe4e0c8
Damião <thiagodamiaosoares@gmail.com>
2024-07-29 12:14:52 -0300

    Update new_dag.py

--- dags/new_dag.py
+++ dags/new_dag.py
@@ -123,7 +123,7 @@ def check_ods_result(**kwargs):
         return 'dummy_task'
 
 dag = DAG(
-    'sinasc_new_test',
+    'SEXO_SINASC',
     timezone='America/Sao_Paulo',
     default_args=default_args,
     description='A DAG to extract and load data from website to MySQL',
